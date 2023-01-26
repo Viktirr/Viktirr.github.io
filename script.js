8011bed6828643b8cf38aad1f5aad69592effc8b
Viktirr <71885195+Viktirr@users.noreply.github.com>
2023-01-26 19:44:57 +0100

    Added favicon
    I think I'm going to redesign this website, it doesn't fit right with me, it looks good as a landing page, but that's about it. I already have an idea on how the redesign should look and I'll hopefully share it within a week or two.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 window.addEventListener("load", () => {
-    document.getElementById("wip-text").classList.add("loaded"); 
+    document.getElementById("hero-wip-text").classList.add("loaded"); 
     document.querySelector("h1").classList.add("loaded"); 
    });
