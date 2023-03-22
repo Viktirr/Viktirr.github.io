eace00fe39095c3f865ddba50db0565cb2a3c701
Viktirr <71885195+Viktirr@users.noreply.github.com>
2023-03-22 01:56:27 +0100

    Updated navigation bar
    Updated the navigation bar and moved the logo to the top of the page. This is part of the redesign, which I'm already late on.

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
+let burgerOpen = false;
+
 window.addEventListener("load", () => {
-    document.getElementById("hero-wip-text").classList.add("loaded"); 
     document.querySelector("h1").classList.add("loaded"); 
-   });
+});
+
+function isBurgerOpen() {
+    element = document.querySelector("nav");
+    burgerElement = document.getElementById("burger-menu");
+    switch(burgerOpen) {
+        case false:
+            burgerElement.classList.add("burgerOpen")
+            element.classList.add("burgerOpen");
+            burgerOpen = true;
+            break;
+        case true:
+            burgerElement.classList.remove("burgerOpen")
+            element.classList.remove("burgerOpen");
+            burgerOpen = false;
+            break;
+    }
+  }
