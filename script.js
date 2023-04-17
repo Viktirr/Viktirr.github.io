43a775b9d05b0234c663b170993d69f67cb072a8
Viktirr <71885195+Viktirr@users.noreply.github.com>
2023-04-18 00:33:57 +0200

    Add button for modal windows
    Added a button for modal windows and text at the top of the window describing what you clicked on. Further smaller optimisation improvements.

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let burgerOpened = false;
 let itemOpened = false;
 
+// Animation for Hero text
 window.addEventListener("load", () => {
     document.querySelector("h1").classList.add("loaded"); 
 });
 
+// Simple hamburger menu handler
 function isBurgerOpen() {
     element = document.querySelector("nav");
     burgerElement = document.getElementById("burger-menu");
@@ -22,7 +24,8 @@ function isBurgerOpen() {
     }
 }
 
-function openItem(itemId) {
+// Upon clicking on an item open a modal window containing a webpage about the item that was clicked on.
+function openItem(itemId, itemName) {
     switch (itemOpened) {
         case false:
             itemOpened = true;
@@ -35,6 +38,9 @@ function openItem(itemId) {
             topBarDiv = document.createElement('div');
             topBarDiv.setAttribute('id', 'topBarDiv');
             topBarDiv.style.margin = '16px';
+
+            itemTitle = document.createElement('h1');
+            itemTitle.innerHTML = itemName;
 
             closeItemImg = document.createElement('img');
             closeItemImg.setAttribute('width', '36');
@@ -52,6 +58,7 @@ function openItem(itemId) {
             fullscreenItemDiv.appendChild(openItemDiv);
             openItemDiv.appendChild(topBarDiv);
             topBarDiv.appendChild(closeItemImg);
+            topBarDiv.appendChild(itemTitle);
             openItemDiv.appendChild(pageIframe);
             setTimeout(function() {
                 openItemDiv.setAttribute('class', 'itemOpened');
@@ -69,3 +76,18 @@ function openItem(itemId) {
             break;
     }
 }
+
+// Close modal window upon pressing escape (doesn't work if the focus is in iframe)
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+      if (itemOpened === true) {
+        itemOpened = false;
+            fullscreenItemDiv = document.getElementById('fullscreenItemDiv');
+            openItemDiv = document.getElementById('openItemDiv');
+            openItemDiv.classList.remove('itemOpened');
+            setTimeout(function() {
+                document.body.removeChild(fullscreenItemDiv);
+            }, 350);
+      }
+    }
+  });
